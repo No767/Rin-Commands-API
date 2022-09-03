82f9d9b6b7cb9d7dd9e5234a9ceeb1ee273a9e62
No767 <73260931+No767@users.noreply.github.com>
2022-09-02 23:53:11 -0700

    Add an Debian-based Dockerfile

---
+++ Debian-Docker/start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ -v POSTGRES_USER ]]; then
+    echo "Postgres_Username=${POSTGRES_USER}" >> /Rin-Commands-API/.env
+else
+    echo "Missing Postgres_Username env var! POSTGRES_USER environment variable is not set."
+    exit 1;
+fi
+
+if [[ -v POSTGRES_PASSWORD ]]; then
+    echo "Postgres_Password=${POSTGRES_PASSWORD}" >> /Rin-Commands-API/.env
+else
+    echo "Missing Postgres_Password env var! POSTGRES_PASSWORD environment variable is not set."
+    exit 1;
+fi
+
+if [[ -v POSTGRES_IP ]]; then
+    echo "Postgres_Server_IP=${POSTGRES_IP}" >> /Rin-Commands-API/.env
+else
+    echo "Missing Postgres_Server_IP env var! POSTGRES_IP environment variable is not set."
+    exit 1;
+fi
+
+if [[ -v POSTGRES_DATABASE ]]; then
+    echo "Postgres_Database=${POSTGRES_DATABASE}" >> /Rin-Commands-API/.env
+else
+    echo "Missing Postgres_Database env var! POSTGRES_DATABASE environment variable is not set."
+    exit 1;
+fi
+
+if [[ -v POSTGRES_PORT ]]; then
+    echo "Postgres_Server_Port=${POSTGRES_PORT}" >> /Rin-Commands-API/.env
+else
+    echo "Missing Postgres_Server_Port env var! POSTGRES_PORT environment variable is not set."
+    exit 1;
+fi
+
+if [[ -v REDIS_IP ]]; then
+    echo "Redis_Server_IP=${REDIS_IP}" >> /Rin-Commands-API/.env
+else
+    echo "Missing Redis_Server_IP env var! REDIS_IP environment variable is not set."
+    exit 1;
+fi
+
+exec gunicorn api:app --workers 5 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
